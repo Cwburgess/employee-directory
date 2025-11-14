a82e689bc7522b24abf6ae58bfb1fb1b4a49e20b
Clint Burgess <38292817+Cwburgess@users.noreply.github.com>
2025-11-14 11:48:03 -0700

    Trigger deployment

--- src/components/layout/EmployeeCard.tsx
+++ src/components/layout/EmployeeCard.tsx
@@ -145,7 +145,7 @@ function nextAnniversary(hire: Date, today: Date, excludeToday = true): Date {
   return nextYear;
 }
 
-/** Years of service on a given anniversary date */
+/** Years of service on a given anniversary date ddd */
 function yearsOnDate(hire: Date, onDate: Date) {
   return onDate.getFullYear() - hire.getFullYear();
 }
